accb00c5d25ec28b575e8b89f366b3858157c52f
Rafael Moura <rafamour03@gmail.com>
2025-11-01 13:13:38 -0300

    fix: adiciona o @Collumn na minha classe

--- demo/src/main/java/com/example/demo/Ninjas/NinjaModel.java
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaModel.java
@@ -25,9 +25,20 @@ public class NinjaModel {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)//Utilize essa anotação com a dependencia chamada "Spring JPA" para trabalhar com persistencia
     // usa-se junto com anotaão @Id, e tem o proposito de buscar uma ordem de preenchimento dos id (Ex. id: 1, id: 2, id: 3 ou id: a, id: b, id: c)
+
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome_ninja")
     private String nome;
+
+    @Column(name = "email", unique = true)
     private String email;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
 
     @JoinColumn(name = "missoes_id") //Junta as duas caracteristicas (Missoes_id - Foreing key ou chave estrageira)
