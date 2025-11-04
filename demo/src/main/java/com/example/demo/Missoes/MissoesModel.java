a189e36e61a67b654cf66d2ca208578b4f88419e
Rafael Moura <rafamour03@gmail.com>
2025-11-03 23:18:21 -0300

    fiadicionando arquivos

---
+++ demo/src/main/java/com/example/demo/Missoes/MissoesModel.java
@@ -0,0 +1,29 @@
+package com.example.demo.Missoes;
+
+import com.example.demo.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+
+@Data //Anotação utilizar os Getters e Setters (Utilize a dependencia Lombok)
+@NoArgsConstructor // Anotação utilizar construtor sem argumento nenhum (Utilize a dependencia Lombok)
+@AllArgsConstructor // Anotação utilizar construtor com todos os argumento (Utilize a dependencia Lombok)
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    //@OneToMany identifica que uma missão tem uma varios ninjas
+    @OneToMany(mappedBy = "missoes")
+    //Utilize o (mappedBy) toda vez que voce for usar o relacionamento de duas tabelas
+    private List<NinjaModel> ninja;
+}
---
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Ninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Anotação controladora Controladora
+@RequestMapping //Anotação onde se coloca as rotas
+public class NinjaController {
+
+    @GetMapping("/boasVindas")//Ele busca a mensagem de que está no metodo "boasvindas" e apresenta no local "Mapeando"
+    public String boasvindas(){
+        return "Testando";
+    }
+
+}
