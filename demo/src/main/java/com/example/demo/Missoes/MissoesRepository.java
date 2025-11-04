fb688c791cbb394b69d647b7766ba30bed61ebbd
Rafael Moura <rafamour03@gmail.com>
2025-11-03 23:17:20 -0300

    fix: altadicionando banco h2 em arquivo dada

--- demo/src/main/java/com/example/demo/Missoes/MissoesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Missoes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
-}
