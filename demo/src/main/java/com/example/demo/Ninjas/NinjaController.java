427dc747aaefc4f16d809221254b423c508be84d
Rafael Moura <rafamour03@gmail.com>
2026-01-20 22:05:59 -0300

    adiconando DTO na Classe ninja

--- demo/src/main/java/com/example/demo/Ninjas/NinjaController.java
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaController.java
@@ -28,21 +28,26 @@ public class NinjaController {
 
     //Mostrar todos os ninjas
     @GetMapping("/listar")
-    public List<NinjaModel> mostrarTodosOsNinjas(){
+    public List<NinjaDTO> mostrarTodosOsNinjas(){
         return ninjaService.listarNinjas();
     }
 
+
     //Mostrar ninja por id
     @GetMapping("/listar/{id}") //{id} - significa um pathvariable (caminho da variavel), entende que o usuario tem que implementar uma informação como um numero
-    public NinjaModel mostrarNinjaPorId(@PathVariable Long id){ // @PathVariable -  trabalha junto com o {id}
+    public NinjaDTO mostrarNinjaPorId(@PathVariable Long id){ // @PathVariable -  trabalha junto com o {id}
         return ninjaService.listarNinjaPorId(id);
     }
 
+
+
     //Alterar dados do ninja
     @PutMapping("/alterar/{id}")
-    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaNovo){
+    public NinjaDTO alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaNovo){
         return ninjaService.ninjaAtualizar(id, ninjaNovo);
     }
+
+
 
     //Deletar Ninja
     @DeleteMapping("/deletar/{id}")
--- demo/src/main/java/com/example/demo/Ninjas/NinjaService.java
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.security.PublicKey;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service // Usa-se na classe service
 public class NinjaService {
@@ -20,14 +21,19 @@ public class NinjaService {
 
 
     //Lista todos os ninjas
-    public List<NinjaModel> listarNinjas(){
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas(){
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
+
     }
 
     //Listar ninjas pot id
-    public NinjaModel listarNinjaPorId(Long id){
+    public NinjaDTO listarNinjaPorId(Long id){
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);  //Optional: usado para caso o id buscado não exista - findById: vem da classe NinjaRepositorio-
-                return ninjaPorId.orElse(null); //orElse:usado para trazer o id buscado ou caso não exista-                // que está extendendo o JPA
+                return ninjaPorId.map(ninjaMapper::map).orElse(null); //orElse:usado para trazer o id buscado ou caso não exista-                // que está extendendo o JPA
                                                         // retorna o null.
     }
 
@@ -44,14 +50,18 @@ public class NinjaService {
        ninjaRepository.deleteById(id);
     }
 
-    //Alterar um cadastro
-    public NinjaModel ninjaAtualizar (Long id, NinjaModel novoNinja){
-        if (ninjaRepository.existsById(id)){
-            novoNinja.setId(id);
-            return ninjaRepository.save(novoNinja);
 
+
+    //Alterar um cadastro
+    public NinjaDTO ninjaAtualizar (Long id, NinjaDTO ninjaDTO){
+        Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id); //Primeiro ele busca o id (Long id)
+        if (ninjaExistente.isPresent()){ //esse metodo pergunta se o id inserido existe, se sim...
+            NinjaModel ninjaAtualizado = ninjaMapper.map(ninjaDTO); //Faz o mapeamento no DTO e...
+            ninjaAtualizado.setId(id); //fazer a edção do id e tbm...
+            NinjaModel ninjaSavo = ninjaRepository.save(ninjaAtualizado); // atualizar a edção no model
+            return  ninjaMapper.map(ninjaSavo); // vai retorna o ninja que foi salvo
         }
-        return null;
+        return null; // se n~çao tiver nada, retorna "null"
     }
 
 }
